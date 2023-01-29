e718b00492877acd01949dc36bcbbaa6d757021b
maxdup <mdupuis@hotmail.ca>
2023-01-28 20:35:37 -0500

    added submodules to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from distutils.core import setup
 setup(
     name='svg_extrude',
-    packages=['svg_extrude', 'svg_extrude.model', 'svg_extrude.scad', 'svg_extrude.util'],
-    version='0.1.0',
+    packages=['svg_extrude', 'svg_extrude.model',
+              'svg_extrude.scad', 'svg_extrude.util', 'libs.cjlano_svg', 'libs.cjlano_svg.svg'],
+    version='0.1.1',
     license='gpl-3.0',
     description='A Python SVG parser and drawing module',
     author='deffi',
     author_email='',
     url='https://github.com/deffi/svg_extrude',
     download_url='',
-    keywords=['svg', 'extrude', 'stl', '3mf', 'amf', 'stl', 'scad', 'openscad'],
+    keywords=['svg', 'extrude', 'stl', '3mf',
+              'amf', 'stl', 'scad', 'openscad'],
     install_requires=['rapidtables', 'tinycss2'],
     classifiers=[
         'Development Status :: 4 - Beta',
